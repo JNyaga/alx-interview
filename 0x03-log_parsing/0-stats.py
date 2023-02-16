c4d40ccb69df2c0e8247925af950abe2865f8cf2
JNyaga <joelnyagamuriithi@gmail.com>
2023-02-17 00:14:56 +0300

    stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,64 +1,60 @@
 #!/usr/bin/python3
+
+"""Reads from standard input and computes metrics.
+
+After every ten lines or the input of a keyboard interruption (CTRL + C),
+prints the following statistics:
+    - Total file size up to that point.
+    - Count of read status codes up to that point.
 """
-Module 0-stats
-script that reads stdin line by line and computes metrics
-"""
-import sys
-import re
-
-total_file_size = 0
-
-# sample_line = '36.196.190.72 - [2022-04-26 07:59:21.687812] '\
-#        '"GET /projects/260 HTTP/1.1" 500 878'
-
-pattern = r'^([\d]{1,3}\.){3}([\d]{1,3})'
-pattern += r'( - )(\[[\d]{4}-[\d]{2}-[\d]{2}'
-pattern += r' [\d]{2}:[\d]{2}:[\d]{2}\.[\d]{1,}\])'
-pattern += r'( "GET \/projects\/260 HTTP\/1\.1") '
-pattern += r'([\d]{3}) ([\d]{1,4})$'
 
 
-codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+def print_stats(size, status_codes):
+    """Print accumulated metrics.
 
-no_of_lines = 0
+    Args:
+        size (int): The accumulated read file size.
+        status_codes (dict): The accumulated count of status codes.
+    """
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
 
 
-def is_line_valid(line: str) -> bool:
-    result = re.match(pattern, line)
-    if result:
-        return True
-    return False
+if __name__ == "__main__":
+    import sys
 
+    size = 0
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
 
-def generate_statistics(line: str) -> None:
-    global total_file_size
-    chars = line.split(' ')
-
-    file_size = int(chars[-1].replace('\n', ''))
     try:
-        status_code = int(chars[-2])
-        if status_code in codes:
-            codes[status_code] += 1
-    except ValueError:
-        pass
+        for line in sys.stdin:
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
+            else:
+                count += 1
 
-    total_file_size += file_size
+            line = line.split()
 
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
 
-def print_statistics() -> None:
-    print('File size: {}'.format(total_file_size))
-    for key, value in sorted(codes.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
 
-if __name__ == '__main__':
-    try:
-        for i, line in enumerate(sys.stdin, 1):
-            # generate statistics only for a valid log
-            if is_line_valid(line):
-                generate_statistics(line)
-            if not i % 10:
-                print_statistics()
+        print_stats(size, status_codes)
+
     except KeyboardInterrupt:
-        print_statistics()
+        print_stats(size, status_codes)
         raise
