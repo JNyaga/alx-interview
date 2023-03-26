8437b0138b992b2bb6ae9e0e4da291f2295378e9
JNyaga <joelnyagamuriithi@gmail.com>
2023-03-26 05:01:08 +0300

    try iterating thru coins first

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -22,9 +22,8 @@ def makeChange(coins, total):
     dp[0] = 0
 
     # start from 1 because we know dp[0]
-    for amount in range(1, total + 1):
-        # test for every coin
-        for coin in coins:
-            if amount - coin >= 0:
-                dp[amount] = min(dp[amount], 1 + dp[amount - coin])
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], 1 + dp[amount - coin])
+
     return dp[total] if dp[total] != total + 1 else -1
